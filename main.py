f11db1306aea120cc1e586d0b3e5eac857203dc2
Zeyad Khaled <zeyadkhaled20016@gmail.com>
2024-01-04 01:42:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from playwright.sync_api import  sync_playwright,Page
 import os
 import pandas as pd
-from datetime import datetime
+from datetime import datetime,timedelta
 from google.oauth2 import service_account
 import gspread
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from googleapiclient.errors import HttpError
 
+def list_pdf_files_in_folder():
+    scope = ['https://www.googleapis.com/auth/drive']
+    service_account_path = os.path.join(os.getcwd(), "helium-10-387902-eb8fa25fda81.json")
+    folder_id = '1aXNw0CQhAY2ObP2bPQOjuyq27co4FSTc'
+    creds = service_account.Credentials.from_service_account_file(service_account_path, scopes=scope)
+    drive_service = build('drive', 'v3', credentials=creds)
+    # List all PDF files in the folder
+    results = drive_service.files().list(q=f"'{folder_id}' in parents and mimeType='application/pdf'", fields="files(name)").execute()
+    files = results.get('files', [])
 
+    return [file['name'] for file in files] if files else []
+
+
+def check_date_relation(input_date_str):
+    # Convert the input date string to a datetime object
+    input_date = datetime.strptime(input_date_str, '%m/%d/%Y')
+    day_before_yesterday = datetime.now() - timedelta(days=2)
+
+    # Compare the input date with today, yesterday, and the day before yesterday
+    return input_date.date() >= day_before_yesterday.date()
 
 def upload_sheet_pdf(file_name):
     scope = ['https://www.googleapis.com/auth/drive']
@@ -64,12 +83,21 @@ def upload_excel(page:Page):
     upload_data_to_google_sheet()
 
 def upload_pdfs(page:Page):
+    page.get_by_text("Created",exact=True).click()
+    page.wait_for_timeout(5000)
+    page.get_by_text("Created",exact=True).click()
+    file_names=list_pdf_files_in_folder()
+    # Get today's date
+    datetime.strptime
+    two_days_ago = datetime.now()-timedelta(days=2)
+    # Format the date as needed
+    two_days_ago_formatted = two_days_ago.strftime('%m/%d/%Y')
     table_rows=page.query_selector_all("colgroup#content-GridcolGroup + tbody > tr")
     for row in table_rows:
         date=row.query_selector("td:nth-child(1)").text_content()
         status=row.query_selector("td:nth-child(9)").text_content().strip()
         print(date,status)
-        if date!=datetime.today().strftime('%m/%d/%Y'):
+        if  not check_date_relation(date) :
             break
         if status!='Approved':
             continue
@@ -83,6 +111,8 @@ def upload_pdfs(page:Page):
             page2.query_selector("body > div > div > div:nth-child(1) > div > div > div:nth-child(1) > div:nth-child(1) > a").click(modifiers=['Alt'])
         download = download_info.value
         pdf_name=f'{proposal_number}_{project_name}.pdf'
+        if pdf_name in file_names:
+            continue
         download.save_as(os.path.join(os.getcwd(), pdf_name))
         upload_sheet_pdf(pdf_name)
         
@@ -90,7 +120,7 @@ def upload_pdfs(page:Page):
     
 def Automation_Process():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
         login(page)
         page.goto("https://app.arbor-note.com/ng/#/sales/report",timeout=100000)
@@ -99,6 +129,7 @@ def Automation_Process():
 
 def main():
     Automation_Process()
+    
     
     
     
